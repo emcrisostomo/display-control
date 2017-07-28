bcbaf6195fe7f0b695b49cd9b31b317a3aece92c
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2017-07-28 13:21:01 +0200

    Add constants to header

---
+++ display-control.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 Enrico M. Crisostomo
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DISPLAY_CONTROL_DISPLAY_CONTROL_H
+#define DISPLAY_CONTROL_DISPLAY_CONTROL_H
+
+static const int DC_OPT_VERSION = 128;
+static const int DC_EXIT_OK = 0;
+static const int DC_EXIT_ILLEGAL_VALUE = 1;
+static const int DC_EXIT_UNKNOWN_OPT = 2;
+
+#endif //DISPLAY_CONTROL_DISPLAY_CONTROL_H
